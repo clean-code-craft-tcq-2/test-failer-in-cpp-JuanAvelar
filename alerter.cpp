5bf4329910b4c9df6da0de61825ea368cc40f26b
Avelar Luna Juan Pablo (BGSW/EGB1-MS) <eva9ga@bosch.com>
2022-02-03 20:36:14 -0600

    I have fixed the alerter

--- alerter.cpp
+++ alerter.cpp
@@ -1,58 +1,68 @@
 #include <iostream>
+#include <cmath>
 #include <assert.h>
-#define CORRECTTHRESHOLD 400
+#define CORRECTTHRESHOLD 410
+#define CORRECTTHRESHOLD2 550
 
 class Alerter{
 protected:
-  virtual int networkAlertStub(float celcius) = 0;
+    virtual int networkAlertStub(float celcius) = 0;
 public:
-  int alertFailureCount = 0;
-  bool alertInCelcius(float farenheit) {
-      bool answer = false;
-      float celcius = (farenheit - 32) * 5 / 9;
-      int returnCode = networkAlertStub(celcius);
-      if (returnCode != 200) {
-          // non-ok response is not an error! Issues happen in life!
-          // let us keep a count of failures to report
-          // However, this code doesn't count failures!
-          // Add a test below to catch this bug. Alter the stub above, if needed.
-          alertFailureCount += 0;
-      }
-      return answer;
-  }
+    size_t alertFailureCount = 0;//if we make this private it could be a way to prevent this from ever resetting
+    bool alertInCelcius(float farenheit) {
+        bool SomethingWentWrong = true;
+        float celcius = (farenheit - 32) * 5 / 9;
+        int returnCode = networkAlertStub(celcius);
+        if (returnCode == 500) {
+            // non-ok response is not an error! Issues happen in life!
+            // let us keep a count of failures to report
+            // However, this code doesn't count failures!
+            // Add a test below to catch this bug. Alter the stub above, if needed.
+            alertFailureCount ++;
+            SomethingWentWrong = false;
+        }
+        else if(returnCode != 200){
+            SomethingWentWrong = false;
+        }
+        return SomethingWentWrong;
+    }
 };
-class Stub1: public Alerter{
+class Stub1Corrected: public Alerter{
 private:
-  int networkAlertStub(float celcius) override {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-  }
+    int networkAlertStub(float celcius) override {
+        std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+        // Return 200 for ok
+        // Return 500 for not-ok
+        // stub always succeeds and returns 200
+        return (celcius < CORRECTTHRESHOLD2) ? 200 : 500 ;
+    }
 };
 class CorrectedStub: public Alerter{
 private:
-  int networkAlertStub(float celcius) override {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return (celcius > CORRECTTHRESHOLD) ? 200 : 500 ;
-  }
+    int networkAlertStub(float celcius) override {
+        std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+        // Return 200 for ok
+        // Return 500 for not-ok
+        // stub always succeeds and returns 200
+        return (celcius < CORRECTTHRESHOLD) ? 200 : 500 ;
+    }
 };
 
 int main() {
-    Alerter* Alarm = new Stub1;
+    size_t TotalAlerts = 0;
+    const float MaxTestTemp = 1400.00;
+    Alerter* Alarm  = new Stub1Corrected;
     Alerter* Alarm2 = new CorrectedStub;
-    for(int Temperature = 0; Temperature < 1400; Temperature+=100){
-        Alarm->alertInCelcius(Temperature);
+    //Design the test to go from 0°f to 1400°f with 100° increments
+    for(float Temperature = 0; Temperature < MaxTestTemp; Temperature+=100){
+        Alarm ->alertInCelcius(Temperature);
         Alarm2->alertInCelcius(Temperature);
+        TotalAlerts++;
     }
-    std::cout << Alarm ->alertFailureCount << " alerts failed.\n";
-    std::cout << Alarm2->alertFailureCount << " alerts failed.\n";
-    assert(Alarm ->alertFailureCount > 0);
-    assert(Alarm2->alertFailureCount > 0);
+    std::cout << Alarm ->alertFailureCount << "/" << TotalAlerts << " alerts failed.\n";
+    std::cout << Alarm2->alertFailureCount << "/" << TotalAlerts << " alerts failed.\n";
+    assert(Alarm ->alertFailureCount == std::floor(TotalAlerts * (1 - (CORRECTTHRESHOLD2*9/5+32) / MaxTestTemp)));
+    assert(Alarm2->alertFailureCount == std::floor(TotalAlerts * (1 - (CORRECTTHRESHOLD *9/5+32) / MaxTestTemp)));
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
